ba4d0ddfc5fa109a6af27b91772b43760bc2e5c1
ymz <ymzcode@163.com>
2021-01-07 11:56:07 +0800

    组件化消息体

--- components/easy-chat/nim-chat-item.vue
+++ components/easy-chat/nim-chat-item.vue
@@ -1,49 +1,34 @@
 <template>
 	<view>
-		<!-- 左侧消息 -->
-		<view class="im-flex cell" v-if="msg.flow === 'in'">
-			<image
-				src="https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1141259048,554497535&fm=26&gp=0.jpg"
-				style="width: 90rpx;height: 90rpx;"
-				class="round"
-				mode="aspectFill"
-			></image>
-			<view>
-				<view class="im-flex">
-					<image src="/static/easy-chat/chat/leftarrow.png" style="width: 40rpx;height: 40rpx;margin-top: 25rpx;" mode="aspectFill"></image>
-					<view class="leftMsg">
-						<text class="text" style="max-width: 450rpx;min-width:210rpx;">{{ msg.text }}</text>
-					</view>
-				</view>
-				<text class="leftDate">{{msg.time | formatTime}}</text>
+		<!-- 整体消息结构 -->
+		<view class="im-flex cell" :style="msgBoxStyle">
+			<!-- 对方的头像 -->
+			<nim-avatar v-if="msg.flow === 'in'" :account="msg.from"></nim-avatar>
+			<!-- 消息盒子 -->
+			<view class="im-flex-column im-align-start">
+				<!-- 消息体 -->
+				<nim-chat-wrapper :flow="msg.flow">
+					<text class="text" style="max-width: 450rpx;min-width:210rpx;">{{ msg.text }}</text>
+				</nim-chat-wrapper>
+				<!-- 消息的时间 -->
+				<text :class="msg.flow === 'in' ? 'leftDate' : 'rightDate' ">{{msg.time | formatTime}}</text>
 			</view>
-		</view>
-		<!-- 右侧消息 -->
-		<view class="im-flex cell" style="justify-content: flex-end;" v-else>
-			<view>
-				<view class="im-flex">
-					<view class="rightMsg">
-						<text class="text" style="max-width: 450rpx;min-width:210rpx;">{{ msg.text }}</text>
-					</view>
-					<image src="/static/easy-chat/chat/rightarrow.png" style="width: 40rpx;height: 40rpx;margin-top: 25rpx;" mode="aspectFill"></image>
-				</view>
-				<text class="rightDate">{{msg.time | formatTime}}</text>
-			</view>
-			<image
-				src="https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3892521478,1695688217&fm=26&gp=0.jpg"
-				style="width: 90rpx;height: 90rpx;"
-				class="round"
-				mode="aspectFill"
-			></image>
+			<!-- 自己的头像 -->
+			<nim-avatar v-if="msg.flow === 'out'" :account="msg.from"></nim-avatar>
 		</view>
 	</view>
 </template>
 
 <script>
+// 引入dayjs 过滤时间
 const dayjs = require('dayjs')
 const relativeTime = require('dayjs/plugin/relativeTime')
 dayjs.extend(relativeTime)
 require('dayjs/locale/zh-cn')
+
+import nimAvatar from '@/components/easy-chat/nim-avatar.vue'
+import nimChatWrapper from '@/components/easy-chat/nim-chat-wrapper.vue'
+
 export default {
 	props: {
 		msg: {
@@ -53,6 +38,20 @@ export default {
 			}
 		}
 	},
+	components: {
+		nimAvatar,
+		nimChatWrapper
+	},
+	computed: {
+		// 控制消息左右显示
+		msgBoxStyle() {
+			let style = ''
+			if (this.msg.flow === 'out') {
+				style += 'justify-content: flex-end;'
+			}
+			return style
+		}
+	},
 	filters: {
 		formatTime(n) {
 			if (dayjs(n).isAfter(dayjs().subtract(24, 'hour'))) {
@@ -60,7 +59,6 @@ export default {
 			} else {
 				return dayjs(n).format('YYYY/MM/DD HH:mm')
 			}
-			
 		}
 	}
 }
@@ -70,24 +68,6 @@ export default {
 .cell {
 	margin-top: 40rpx;
 	padding: 0 20rpx;
-}
-
-.leftMsg,
-.rightMsg {
-	padding: 20rpx;
-	border-radius: 10rpx;
-	margin-top: 10rpx;
-	flex: 1;
-}
-
-.leftMsg {
-	background-color: #fff;
-	margin-left: -15rpx;
-}
-
-.rightMsg {
-	background-color: #96eb6a;
-	margin-right: -15rpx;
 }
 
 .text {
---
+++ components/easy-chat/nim-chat-wrapper.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="im-flex">
+		<!-- 左箭头 -->
+		<image v-if="flow === 'in'" src="/static/easy-chat/chat/leftarrow.png" style="width: 40rpx;height: 40rpx;margin-top: 25rpx;" mode="aspectFill"></image>
+		<!-- 插入的消息主体 -->
+		<view :class="msgWrapperClass">
+			<slot></slot>
+		</view>
+		<!-- 右箭头 -->
+		<image v-if="flow === 'out'" src="/static/easy-chat/chat/rightarrow.png" style="width: 40rpx;height: 40rpx;margin-top: 25rpx;" mode="aspectFill"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			// 消息的流向 in 收到 out 发出
+			flow: {
+				type: String,
+				default: 'in'
+			}
+		},
+		computed: {
+			msgWrapperClass() {
+				if (this.flow === 'in') {
+					return 'leftMsg'
+				} else {
+					return 'rightMsg'
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped>
+.leftMsg,
+.rightMsg {
+	padding: 20rpx;
+	border-radius: 10rpx;
+	margin-top: 10rpx;
+	flex: 1;
+}
+
+.leftMsg {
+	background-color: #fff;
+	margin-left: -15rpx;
+}
+
+.rightMsg {
+	background-color: #96eb6a;
+	margin-right: -15rpx;
+}
+</style>
