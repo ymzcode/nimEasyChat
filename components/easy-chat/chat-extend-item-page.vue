9be07a30a7abc3040b2d7376a87ca4226c81c0d3
ymz <ymzcode@163.com>
2021-01-08 14:17:45 +0800

    添加扩展菜单

---
+++ components/easy-chat/chat-extend-item-page.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="im-flex-column">
+		<swiper
+		 style="height: 450rpx;"
+		 :circular="false"
+		 :indicator-dots="true"
+		 indicator-active-color="#ffffff"
+		 >
+			<swiper-item class="im-flex">
+				<text>swiper1</text>
+			</swiper-item>
+			<swiper-item class="im-flex">
+				<text>swiper2</text>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ components/easy-chat/nim-chat-extend-menu.vue
@@ -0,0 +1,23 @@
+<template>
+	<view v-if="$attrs.show" class="im-flex-column im-border-top">
+		<chat-extend-item-page v-if="$attrs.type === 'extend'"></chat-extend-item-page>
+	</view>
+</template>
+
+<script>
+import chatExtendItemPage from '@/components/easy-chat/chat-extend-item-page.vue'
+export default {
+	data() {
+		return {
+			type: this.$attrs.type,
+			show: this.$attrs.show
+		}
+	},
+	components: {
+		chatExtendItemPage
+	}
+}
+</script>
+
+<style>
+</style>
