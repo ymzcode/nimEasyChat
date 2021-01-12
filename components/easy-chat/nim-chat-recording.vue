0af97f796464f86015c73455d7f5e5e22496335f
ymz <ymzcode@163.com>
2021-01-12 18:26:54 +0800

    语音录制组件完善

--- components/easy-chat/nim-chat-recording.vue
+++ components/easy-chat/nim-chat-recording.vue
@@ -5,11 +5,17 @@
 </template>
 
 <script>
+	const recorderManager = uni.getRecorderManager();
+
 	export default {
 		data() {
 			return {
 				// 是否正在录制中
-				isRecording: false
+				isRecording: false,
+				// 录制的文件地址
+				voicePath: '',
+				// 判断录音文件是否有效
+				fileOk: false
 			}
 		},
 		computed: {
@@ -20,16 +26,64 @@
 				} else {
 					return '按住  说话'
 				}
+			},
+			// 当前会话的id
+			currentSessionId() {
+				return this.$store.getters['initNim/currentSessionId']
 			}
+		},
+		mounted() {
+			let _self = this;
+			// 注册停止事件
+			recorderManager.onStop(function (res) {
+				if (!_self.fileOk) {
+					uni.showToast({
+						title: '说话时间过短，请重新录制',
+						icon: 'none'
+					})
+					return ;
+				}
+				_self.voicePath = res.tempFilePath;
+				console.log('录制结束' , res);
+				_self.$store.dispatch('initNim/nimSendFile', {
+					type: 'audio',
+					filePath: res.tempFilePath,
+					scene: _self.currentSessionId.split('-')[0],
+					to: _self.currentSessionId.split('-')[1]
+				});
+			});
+			// 注册录音错误事件
+			recorderManager.onError(function (error) {
+				console.error('录制出现错误', error);
+			})
 		},
 		methods: {
 			touchstart(e) {
 				console.log('touchstart', e);
 				this.isRecording = true
+				this.startRecord()
+				e.stopPropagation()
 			},
 			touchend(e) {
 				console.log('touchend', e);
 				this.isRecording = false
+				this.endRecord()
+				e.stopPropagation()
+			},
+			startRecord() {
+				console.log('开始录音');
+				this.fileOk = false
+				recorderManager.start({
+					duration: 1000 * 60 ,// 1分钟
+					format: 'mp3',
+				});
+				setTimeout(() => {
+					this.fileOk = true
+				}, 1500)
+			},
+			endRecord() {
+				console.log('录音结束');
+				recorderManager.stop();
 			}
 		}
 	}
