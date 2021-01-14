820fe155a3c95fe2bf3f22edf3ea41f976f07ddf
ymz <ymzcode@163.com>
2021-01-14 10:49:42 +0800

    完善文本

--- components/easy-chat/chat-item-text.vue
+++ components/easy-chat/chat-item-text.vue
@@ -1,8 +1,8 @@
 <template>
-	<view class="im-flex im-flex-wrap im-position-relative " :style="boxStyle" style="max-width: 450rpx;min-width: 230rpx; height: auto;">
+	<view class="im-flex im-flex-wrap im-position-relative" :style="boxStyle" style="max-width: 450rpx;min-width: 230rpx; height: auto;">
 		<view v-for="(textVal, textIndex) in textNode" :key="textIndex">
 			<image v-if="textVal.type === 'img'" :src="textVal.src" mode="aspectFit" style="width:40rpx;height:40rpx;"></image>
-			<text v-else-if="textVal.type === 'text'" space="emsp" class="im-font-30" style="max-width: 450rpx;word-wrap: break-word;word-break: break-all;">{{ textVal.text }}</text>
+			<text v-else-if="textVal.type === 'text'" space="emsp" class="im-font-30 im-flex im-flex-wrap" style="max-width: 450rpx;word-wrap: break-word;word-break: break-all;">{{ textVal.text }}</text>
 		</view>
 		<!-- 不解析表情的文本样式 -->
 		<!-- <text class="im-font-30 im-flex-1" style="max-width: 450rpx;min-width:210rpx;word-wrap: break-word;word-break: break-all;">{{ $attrs.msg.text }}</text> -->
