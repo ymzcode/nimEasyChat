bca75dd20d0d5e611c85abb5a059a9cf4ba00e50
ymz <ymzcode@163.com>
2021-01-14 17:27:05 +0800

    更改样式

--- components/easy-chat/nim-team-member.vue
+++ components/easy-chat/nim-team-member.vue
@@ -1,5 +1,5 @@
 <template>
-	<view class="im-flex-column im-bg-white im-py-2 im-mt-3 im-px-3" @tap="onClick">
+	<view class="im-flex-column im-bg-white im-py-3 im-mt-3 im-px-3" @tap="onClick">
 		<view class="im-flex im-align-center im-justify-between">
 			<text class="im-font-32 im-font-black">群成员</text>
 			<text class="im-font-light im-font-25">{{ $attrs.detailInfo.memberNum }}人 ></text>
