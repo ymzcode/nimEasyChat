033ff2272bc6187c5438caa6b4ad0075ece32a8f
ymz <ymzcode@163.com>
2021-01-14 16:10:00 +0800

    添加查找聊天内容组件

---
+++ components/easy-chat/nim-search-history.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="im-flex-column im-bg-white im-py-2 im-mt-3 im-px-3">
+		<text class="im-font-32 im-font-black">查找聊天内容</text>
+		<!-- 每一项 -->
+		<view class="im-flex im-align-center im-justify-around im-my-3">
+			<view class="im-flex-column im-align-center im-mx-4">
+				<image src="/static/easy-chat/details/search@2x.png" mode="aspectFill" style="width: 56rpx;height: 56rpx;"></image>
+				<text class="im-font-28 im-font-light im-mt-2">搜索</text>
+			</view>
+			<view class="im-flex-column im-align-center im-mx-4">
+				<image src="/static/easy-chat/details/image@2x.png" mode="aspectFill" style="width: 56rpx;height: 56rpx;"></image>
+				<text class="im-font-28 im-font-light im-mt-2">图片</text>
+			</view>
+			<view class="im-flex-column im-align-center im-mx-4">
+				<image src="/static/easy-chat/details/file@2x.png" mode="aspectFill" style="width: 56rpx;height: 56rpx;"></image>
+				<text class="im-font-28 im-font-light im-mt-2">文件</text>
+			</view>
+			<view class="im-flex-column im-align-center im-mx-4">
+				<image src="/static/easy-chat/details/link@2x.png" mode="aspectFill" style="width: 56rpx;height: 56rpx;"></image>
+				<text class="im-font-28 im-font-light im-mt-2">链接</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
