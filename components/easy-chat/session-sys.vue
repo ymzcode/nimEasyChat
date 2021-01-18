0f1a30711efef7f6112c6316fa972fa482ce5b81
ymz <ymzcode@163.com>
2021-01-18 17:20:49 +0800

    完善系统消息列表显示

--- components/easy-chat/session-sys.vue
+++ components/easy-chat/session-sys.vue
@@ -7,12 +7,12 @@
 			<view class="im-flex-column im-flex-1 im-px-2">
 				<view class="im-flex im-flex-1 im-justify-between im-align-center">
 					<text class="im-font-32 im-font-black">系统消息</text>
-					<text class="im-font-25 im-font-light">12月10日</text>
+					<text class="im-font-25 im-font-light">{{ newMsg.time | formatTime }}</text>
 				</view>
 				<view class="im-flex im-flex-1 im-justify-between im-align-center">
-					<text class="im-font-28 im-text-ellipsis im-font-light" style="max-width: 540rpx;">新系统消息</text>
-					<view class="im-bg-red-2 im-round-full im-flex im-justify-center im-align-center" style="width: 35rpx;height: 35rpx;">
-						<text class="im-font-23 im-font-white">90</text>
+					<text class="im-font-28 im-text-ellipsis im-font-light" style="max-width: 540rpx;">{{ detailInfo }}</text>
+					<view v-if="systemUnreadNum > 0" class="im-bg-red-2 im-round-full im-flex im-justify-center im-align-center" style="width: 35rpx;height: 35rpx;">
+						<text class="im-font-23 im-font-white">{{ systemUnreadNum }}</text>
 					</view>
 				</view>
 			</view>
@@ -21,6 +21,60 @@
 </template>
 
 <script>
+	import useDayjs from '@/common/NIM/useDayjs.js'
+	export default {
+		data() {
+			return {
+				systemUnreadNum: 0
+			}
+		},
+		computed: {
+			systemMsgArr() {
+				this.updateSystemUnreadNum()
+				return this.$store.getters['initNim/systemMsgArr']
+			},
+			newMsg() {
+				let sysmsg = this.systemMsgArr[0]
+				if (sysmsg) {
+					sysmsg = Object.assign({}, sysmsg)
+					sysmsg.content = JSON.parse(sysmsg.content)
+					return sysmsg
+				} else {
+					return {}
+				}
+			},
+			// 要展示的详情内容
+			detailInfo() {
+				let text = '暂无系统消息'
+				if (this.newMsg.content) {
+					let content = this.newMsg.content
+					switch(content.type) {
+						case 'tipSysMsg':
+							text = content.data.detail
+							break;
+					}
+				}
+				return text
+			}
+		},
+		mounted() {
+			this.updateSystemUnreadNum()
+		},
+		filters: {
+			formatTime(n) {
+				if (!n) {
+					return ''
+				}
+				return useDayjs.formatRelative24(n)
+			}
+		},
+		methods: {
+			updateSystemUnreadNum() {
+				console.log('更新系统消息的未读数');
+				this.systemUnreadNum = uni.getStorageSync('systemUnreadNum') || 0
+			}
+		}
+	}
 </script>
 
 <style>
