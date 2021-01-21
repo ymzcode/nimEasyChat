e203d149183e8f3a1d5c4c5940d9cb03cd81a8e8
吃饱了不饿 <68903346+ymzcode@users.noreply.github.com>
2021-01-21 19:12:27 +0800

    展示提醒消息

--- components/easy-chat/nim-chat-item.vue
+++ components/easy-chat/nim-chat-item.vue
@@ -1,42 +1,50 @@
 <template>
 	<view class="im-position-relative">
-		<!-- 整体消息结构 -->
-		<view class="im-flex cell" :style="msgBoxStyle">
-			<!-- 对方的头像 -->
-			<nim-avatar v-if="msg.flow === 'in'" :account="msg.from"></nim-avatar>
-			<!-- 消息盒子 -->
-			<view class="im-flex-column im-align-start">
-				<!-- 消息体 -->
-				<nim-chat-wrapper :flow="msg.flow" :type="msg.type" :msg="msg" v-on="$listeners">
-					<!-- 普通文本消息 -->
-					<chat-item-text v-if="msg.type === 'text'" :msg="msg"></chat-item-text>
-					<!-- 图片消息 -->
-					<chat-item-image v-else-if="msg.type === 'image'" :msg="msg"></chat-item-image>
-					<!-- 视频消息 -->
-					<chat-item-video v-else-if="msg.type === 'video'" :msg="msg"></chat-item-video>
-					<!-- 语音消息 -->
-					<chat-item-audio v-else-if="msg.type === 'audio'" :msg="msg"></chat-item-audio>
-					<!-- 自定义消息 -->
-					<template v-else-if="msg.type === 'custom'">
-						<!-- 跳转消息类型 -->
-						<chat-item-navigate v-if="customType === 'votes'" :msg="msg"></chat-item-navigate>
-						<chat-item-msgcard v-else-if="customType === 'msgCard'" :msg="msg"></chat-item-msgcard>
-						<text v-else class="im-font-28">未定义的类型</text>
-					</template>
+		<!-- 展示提醒消息 -->
+		<template v-if="msg.type === 'tip'">
+			<chat-item-tipmsg :msg="msg"></chat-item-tipmsg>
+		</template>
+		
+		<template v-else>
+			<!-- 整体消息结构 -->
+			<view class="im-flex cell" :style="msgBoxStyle">
+				<!-- 对方的头像 -->
+				<nim-avatar v-if="msg.flow === 'in'" :account="msg.from"></nim-avatar>
+				<!-- 消息盒子 -->
+				<view class="im-flex-column im-align-start">
+					<!-- 消息体 -->
+					<nim-chat-wrapper :flow="msg.flow" :type="msg.type" :msg="msg" v-on="$listeners">
+						<!-- 普通文本消息 -->
+						<chat-item-text v-if="msg.type === 'text'" :msg="msg"></chat-item-text>
+						<!-- 图片消息 -->
+						<chat-item-image v-else-if="msg.type === 'image'" :msg="msg"></chat-item-image>
+						<!-- 视频消息 -->
+						<chat-item-video v-else-if="msg.type === 'video'" :msg="msg"></chat-item-video>
+						<!-- 语音消息 -->
+						<chat-item-audio v-else-if="msg.type === 'audio'" :msg="msg"></chat-item-audio>
+						<!-- 自定义消息 -->
+						<template v-else-if="msg.type === 'custom'">
+							<!-- 跳转消息类型 -->
+							<chat-item-navigate v-if="customType === 'votes'" :msg="msg"></chat-item-navigate>
+							<chat-item-msgcard v-else-if="customType === 'msgCard'" :msg="msg"></chat-item-msgcard>
+							<text v-else class="im-font-28">未定义的类型</text>
+						</template>
+						
+					</nim-chat-wrapper>
+					<!-- 消息体下方显示栏 -->
+					<view class="im-flex im-align-center im-mt-1">
+						<!-- 单聊消息的已读未读状态显示 -->
+						<text v-if="msg.flow === 'out'" class="im-font-23 im-font-light">{{$attrs.nowSessionInfo.msgReceiptTime && $attrs.nowSessionInfo.msgReceiptTime > msg.time ? '已读' : '未读' }}</text>
+						<!-- 消息的时间 -->
+						<text :class="msg.flow === 'in' ? 'leftDate' : 'rightDate' ">{{msg.time | formatTime}}</text>
+					</view>
 					
-				</nim-chat-wrapper>
-				<!-- 消息体下方显示栏 -->
-				<view class="im-flex im-align-center im-mt-1">
-					<!-- 单聊消息的已读未读状态显示 -->
-					<text v-if="msg.flow === 'out'" class="im-font-23 im-font-light">{{$attrs.nowSessionInfo.msgReceiptTime && $attrs.nowSessionInfo.msgReceiptTime > msg.time ? '已读' : '未读' }}</text>
-					<!-- 消息的时间 -->
-					<text :class="msg.flow === 'in' ? 'leftDate' : 'rightDate' ">{{msg.time | formatTime}}</text>
 				</view>
-				
+				<!-- 自己的头像 -->
+				<nim-avatar v-if="msg.flow === 'out'" :account="msg.from"></nim-avatar>
 			</view>
-			<!-- 自己的头像 -->
-			<nim-avatar v-if="msg.flow === 'out'" :account="msg.from"></nim-avatar>
-		</view>
+		</template>
+		
 	</view>
 </template>
 
@@ -49,6 +57,7 @@ import chatItemImage from '@/components/easy-chat/chat-item-image.vue'
 import chatItemVideo from '@/components/easy-chat/chat-item-video.vue'
 import chatItemAudio from '@/components/easy-chat/chat-item-audio.vue'
 import chatItemText from '@/components/easy-chat/chat-item-text.vue'
+import chatItemTipmsg from '@/components/easy-chat/chat-item-tipMsg.vue'
 
 import useDayjs from '@/common/NIM/useDayjs.js'
 
@@ -73,7 +82,8 @@ export default {
 		chatItemImage,
 		chatItemVideo,
 		chatItemAudio,
-		chatItemText
+		chatItemText,
+		chatItemTipmsg
 	},
 	computed: {
 		// 控制消息左右显示
