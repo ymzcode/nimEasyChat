923bf98967c608108c691c031b303041dd403160
吃饱了不饿 <68903346+ymzcode@users.noreply.github.com>
2021-01-22 15:35:50 +0800

    提取会话头像

---
+++ components/easy-chat/session-item-avatar.vue
@@ -0,0 +1,19 @@
+<template>
+	<nim-avatar v-if="$attrs.scene === 'p2p'" :account="$attrs.to"></nim-avatar>
+	<team-avatar v-else :teamId="$attrs.to"></team-avatar>
+</template>
+
+<script>
+	import nimAvatar from '@/components/easy-chat/nim-avatar.vue'
+	import teamAvatar from '@/components/easy-chat/team-avatar.vue'
+	
+	export default {
+		components: {
+			nimAvatar,
+			teamAvatar
+		}
+	}
+</script>
+
+<style>
+</style>
