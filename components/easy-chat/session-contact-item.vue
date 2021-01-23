a5728a33ab12feec323e29390aff584802c6e274
吃饱了不饿 <68903346+ymzcode@users.noreply.github.com>
2021-01-23 16:27:59 +0800

    调整样式 增加小箭头

--- components/easy-chat/session-contact-item.vue
+++ components/easy-chat/session-contact-item.vue
@@ -1,13 +1,22 @@
 <template>
-	<view class="im-flex im-align-center im-ml-3 im-pr-3 im-py-2 im-border-bottom" @tap="onClick">
-		<session-item-avatar :scene="$attrs.session.scene" :to="$attrs.session.to"></session-item-avatar>
-		<text class="im-font-32 im-ml-2 im-text-ellipsis" style="max-width: 550rpx;">{{sessionName}}</text>
+	<view class="im-flex im-justify-between im-align-center im-ml-3 im-pr-3 im-py-2 im-border-bottom" @tap="onClick">
+		<view class="im-flex im-align-center im-flex-1">
+			<session-item-avatar :scene="$attrs.session.scene" :to="$attrs.session.to"></session-item-avatar>
+			<text class="im-font-32 im-ml-2 im-text-ellipsis im-flex-1">{{sessionName}}{{sessionName}}{{sessionName}}{{sessionName}}{{sessionName}}{{sessionName}}{{sessionName}}{{sessionName}}{{sessionName}}</text>
+		</view>
+		<image v-if="showIcon" src="/static/easy-chat/chat/arrow-right.png" mode="aspectFill" style="width: 24rpx;height: 24rpx;"></image>
 	</view>
 </template>
 
 <script>
 	import sessionItemAvatar from '@/components/easy-chat/session-item-avatar.vue'
 	export default {
+		props: {
+			showIcon: {
+				type: Boolean,
+				default: false
+			}
+		},
 		computed: {
 			userObj() {
 				return this.$store.getters['initNim/userObj']
