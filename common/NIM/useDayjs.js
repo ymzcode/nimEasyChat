376e8e44a23b8e9c1fdccfa5211b688223ccb958
吃饱了不饿 <68903346+ymzcode@users.noreply.github.com>
2021-01-25 15:03:38 +0800

    普通格式化

--- common/NIM/useDayjs.js
+++ common/NIM/useDayjs.js
@@ -7,11 +7,21 @@ require('dayjs/locale/zh-cn')
 let useDayjs = {
 	// 只友好化显示24小时内的日期
 	formatRelative24(time) {
+		if (!time) {
+			return null
+		}
 		if (dayjs(time).isAfter(dayjs().subtract(24, 'hour'))) {
 			return dayjs(time).locale('zh-cn').fromNow()
 		} else {
 			return dayjs(time).format('YYYY/MM/DD HH:mm')
 		}
+	},
+	// 普通格式化时间
+	defaultFormat(time) {
+		if (!time) {
+			return null
+		}
+		return dayjs(time).format('YYYY/MM/DD HH:mm')
 	}
 }
 
