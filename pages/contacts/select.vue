e53441d21f89bfa271bd61a7776f55dc870b7a5c
吃饱了不饿 <68903346+ymzcode@users.noreply.github.com>
2021-01-27 17:20:00 +0800

    模拟选择联系人

---
+++ pages/contacts/select.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="">
+		选择联系人
+		<button type="default" @tap="add">添加人</button>
+	</view>
+</template>
+
+<script>
+	import handleBusiness from '@/common/NIM/handleBusiness.js'
+	export default {
+		data() {
+			return {
+				teamId: ''
+			}
+		},
+		onLoad(event) {
+			console.log(event);
+			this.teamId = event.teamId
+		},
+		methods: {
+			add() {
+				console.log('添加成员');
+				handleBusiness.team.addMember(this.teamId, '137')
+				uni.navigateBack()
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
