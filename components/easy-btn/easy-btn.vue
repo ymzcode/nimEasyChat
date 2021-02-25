4475218246cfe91e82f9cd1bf1f6e9f9512703da
ymz <ymzcode@163.com>
2021-02-25 15:10:04 +0800

    使用模拟点击

--- components/easy-btn/easy-btn.vue
+++ components/easy-btn/easy-btn.vue
@@ -1,5 +1,5 @@
 <template>
-	<view ref="easyBtn" hover-class="im-bg-black-1" class="im-flex im-bg-black-2 im-align-center im-justify-center" :style="btnStyle" @tap="onClick">
+	<view ref="easyBtn" hover-class="im-bg-black-1" class="im-flex im-bg-black-2 im-align-center im-justify-center" :style="btnStyle" @touchstart="onTouchStart" @touchend="onTouchEnd">
 		<slot>
 			<text class="im-font-30 im-font-white">{{ btnText }}</text>
 		</slot>
@@ -34,6 +34,14 @@
 				return style
 			}
 		},
+		data() {
+			return {
+				touchXY: {
+					pageX: 0,
+					pageY: 0
+				}
+			}
+		},
 		mounted() {
 			this.showAn()
 		},
@@ -41,8 +49,21 @@
 			this.hideAn()
 		},
 		methods: {
-			onClick() {
-				this.$emit('onClick')
+			onTouchStart(e) {
+				// console.log(e);
+				this.touchXY.pageX = e.changedTouches[0].pageX
+				this.touchXY.pageY = e.changedTouches[0].pageY
+			},
+			onTouchEnd(e) {
+				// console.log(e);
+				if (Math.abs(e.changedTouches[0].pageX - this.touchXY.pageX) > 10 || Math.abs(e.changedTouches[0].pageY - this.touchXY.pageY) > 10) {
+					return ;
+				}
+				
+				this.$emit('onClick', e)
+			},
+			onClick(e) {
+				this.$emit('onClick', e)
 			},
 			showAn() {
 				let _self = this
